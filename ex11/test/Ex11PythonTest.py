255a12c8204f7027d69dd0e458f134224fd0ec89
Qba91 <jakubgawelczyk@o2.pl>
2015-05-28 20:32:55 +0200

    utworzono test - wynik niepowodzenie

---
+++ ex11/test/Ex11PythonTest.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on 28-05-2015
+
+@author: user
+'''
+import unittest
+from ex11.Ex11Python import next_number 
+
+
+class Ex11PythonTest(unittest.TestCase):
+    
+    def TestRulesForTheEvenNumber(self):
+        self.assertEqual(next_number(2), 1)  
+    
+    def TestRulesForTheOddNumber(self):
+        self.assertEqual(next_number(3), 10)
+        
+
+if __name__ == '__main__':
+    unittest.main()
