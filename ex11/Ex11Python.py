64048fff592811bf43982394490b1e194fac9ec2
Qba91 <jakubgawelczyk@o2.pl>
2015-05-28 21:00:52 +0200

    dudano nowe testy metod: sequence longestCollatzSequence

--- ex11/Ex11Python.py
+++ ex11/Ex11Python.py
@@ -1,6 +1,9 @@
 # -*- coding: UTF-8 -*-
 
 def next_number(n):
+    """
+    Zwraca nastepny element sekwencji zogdnie z warunkami
+    """
     if n % 2== 0:
         return n/2
     else:
--- ex11/test/Ex11PythonTest.py
+++ ex11/test/Ex11PythonTest.py
@@ -6,6 +6,8 @@ Created on 28-05-2015
 '''
 import unittest
 from ex11.Ex11Python import next_number 
+from ex11.Ex11Python import sequence
+from ex11.Ex11Python import longestCollatzSequence
 
 class Ex11PythonTest(unittest.TestCase):
     
@@ -14,7 +16,14 @@ class Ex11PythonTest(unittest.TestCase):
     
     def TestRulesForTheOddNumber(self):
         self.assertEqual(next_number(3), 10)
-        
+    
+    def TestSequence(self):
+        result = sequence(13)
+        self.assertEqual(result, 10)
+
+    def TestSequenceLongestCollatzSequence(self):
+        result = longestCollatzSequence()
+        self.assertEqual(result, 837799)   
 
 if __name__ == '__main__':
     unittest.main()
